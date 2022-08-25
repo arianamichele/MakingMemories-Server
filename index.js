107cb5f1f4904b45cc3c79fa7bce0c7c4064aca5
YOUR-USERNAME <arianamichelebooking@gmail.com>
2022-08-25 13:28:49 -0700

    rearraged routes

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://arianamichele:access123@cluster0.tlabt.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
