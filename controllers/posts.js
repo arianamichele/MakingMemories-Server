d5f6277eb54a22752fa3f7940da1bdb3f18118f1
YOUR-USERNAME <arianamichelebooking@gmail.com>
2022-09-06 10:00:04 -0700

    debugging

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,75 +1,39 @@
-import express from 'express';
 import mongoose from 'mongoose';
-
 import PostMessage from '../models/postMessage.js';
 
-const router = express.Router();
-
 export const getPosts = async (req, res) => {
-    const { page } = req.query;
-
     try {
-        const LIMIT = 8;
-        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
-    
-        const total = await PostMessage.countDocuments({});
-        const posts = await PostMessage.find().limit(LIMIT).skip(startIndex);
+        const postMessages = await PostMessage.find();
 
-        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
-    } catch (error) {    
-        res.status(404).json({ message: error.message });
-    }
-}
+        console.log(postMessages);
 
-export const getPostsBySearch = async (req, res) => {
-    const { searchQuery } = req.query;
-
-    try {
-        const title = new RegExp(searchQuery, "i");
-    
-        const posts = await PostMessage.find({ title });
-
-        res.json({ data: posts });
-    } catch (error) {    
-        res.status(404).json({ message: error.message });
-    }
-}
-
-export const getPost = async (req, res) => { 
-    const { id } = req.params;
-
-    try {
-        const post = await PostMessage.findById(id);
-        
-        res.status(200).json(post);
+        res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-}
+};
 
 export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() })
+    const newPost = new PostMessage(post);
 
     try {
-        await newPostMessage.save();
+        await newPost.save();
 
-        res.status(201).json(newPostMessage );
+        res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        res.status(409).json({ message: error.message })    
     }
 }
 
 export const updatePost = async (req, res) => {
-    const { id } = req.params;
-    const { title, message, creator, selectedFile, tags } = req.body;
-    
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    const { id: _id } = req.params;
+    const post = req.body;
 
-    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
     res.json(updatedPost);
 }
@@ -77,35 +41,22 @@ export const updatePost = async (req, res) => {
 export const deletePost = async (req, res) => {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
     await PostMessage.findByIdAndRemove(id);
 
-    res.json({ message: "Post deleted successfully." });
+    console.log('delete')
+
+    res.json({ message: 'Post deleted successfully' })
 }
 
-export const likePost = async (req, res) => {
+export const likePost = async (req,res) => {
     const { id } = req.params;
 
-    if (!req.userId) {
-        return res.json({ message: "Unauthenticated" });
-      }
-
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
     
     const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
 
-    const index = post.likes.findIndex((id) => id ===String(req.userId));
-
-    if (index === -1) {
-      post.likes.push(req.userId);
-    } else {
-      post.likes = post.likes.filter((id) => id !== String(req.userId));
-    }
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
-    res.status(200).json(updatedPost);
+    res.json(updatedPost);
 }
-
-
-
-export default router;
